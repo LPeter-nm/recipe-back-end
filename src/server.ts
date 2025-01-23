62bc2162a1faab0ab09256e8e9a5e58931eb5f73
Jhoão <jhpdrnsantos2@gmail.com>
2025-01-22 21:46:23 -0300

    chore: Alterado porta do server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { RecipeRoute } from './routes/RouteRecipe'
 import { UserRoute } from './routes/RouteUser'
 const app = express()
-const port = 3000
+const port = 4000
 
 app.use(express.json())
 app.use(UserRoute)
