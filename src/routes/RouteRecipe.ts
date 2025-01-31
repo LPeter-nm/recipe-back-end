66ae4e376c76f0f61a7c0b6aad4f57200422c2b5
Jhoão <jhpdrnsantos2@gmail.com>
2025-01-31 09:12:26 -0300

    fix: Erro ao deletar receita

--- src/routes/RouteRecipe.ts
+++ src/routes/RouteRecipe.ts
@@ -6,8 +6,8 @@ const routes = Router();
 routes.post('/recipe', authMiddleware, CreateRecipe)
 routes.get('/recipe/:id', authMiddleware, ShowRecipe)
 routes.get('/recipes', authMiddleware,IndexRecipe)
-routes.put('/recipe/:id', authMiddleware, UpdateRecipe)
-routes.patch('/recipe/favorite/:id', authMiddleware, updateFavorite)
+routes.patch('/recipe/:id', authMiddleware, UpdateRecipe)
+routes.put('/recipe/favorite/:id', authMiddleware, updateFavorite)
 routes.delete('/recipe/:id', authMiddleware, DeleteRecipe)
 
 export {routes as RecipeRoute}
